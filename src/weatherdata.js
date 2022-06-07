76394f15ed4429f098602916a517f8879230ec95
Raquel Olaya <raquelolaya@Raquels-MacBook-Air.local>
2022-06-07 14:26:57 -0500

    getting the weather data

---
+++ src/weatherdata.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        img:"http://res.cloudinary.com/jkeohan/image/uplhttpoad/v1535732381/day.svg" alt="",
+        conditions: "clear",
+        time: "day",
+    }
+    {
+        img:"http://res.cloudinary.com/jkeohan/image/uplhttpoad/v1535732381/day.svg" alt="",
+        conditions: "clear",
+        time: "day",
+    }
+    {
+        img:"http://res.cloudinary.com/jkeohan/image/uplhttpoad/v1535732381/day.svg" alt="",
+        conditions: "clear",
+        time: "day",
+    }
+    {
+        img:"http://res.cloudinary.com/jkeohan/image/uplhttpoad/v1535732381/day.svg" alt="",
+        conditions: "clear",
+        time: "day",
+    }
+    {
+        img:"http://res.cloudinary.com/jkeohan/image/uplhttpoad/v1535732381/day.svg" alt="",
+        conditions: "clear",
+        time: "day",
+    }
+]
